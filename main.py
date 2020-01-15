52f18a7ff6ca3d38652c2c50fac521a09ecfbbf5
diegogarromolina <diego.garromolina@ucr.ac.cr>
2020-01-15 01:41:55 -0600

    Se reemplaza el archivo main.py por el archivo main.py de la primera version, este archivo es el que descarga todos los METARES desde Ogimet.com

--- main.py
+++ main.py
@@ -1,40 +1,104 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 #-*-coding: utf-8-*-
 
-import pandas as pd
-import numpy as np
+from datetime import datetime
+from calendar import monthrange
+from time import sleep
+import re
+import urllib.request
 
-data = pd.read_csv('metar_data.csv')
-print(data.head(10))
-print(data.shape)
+def convertir_a_string(valor):
+	if valor < 10:
+		string = '0' + str(valor)
+	else:
+		string = str(valor)
+	return string
 
-fechas = [8, 9, 10, 11, 12, 13, 14, 15]
-subset = data[(data['DIA'] >= fechas[0]) & (data['DIA'] <= fechas[-1])]
-print(subset.head())
-print(subset.shape)
-subset = subset[subset['MES'] == 1]
-print(subset.head())
-print(subset.shape)
-qnhs = [29.99, 30.05]
-subset = subset[(subset['QNH'] >= qnhs[0]) & (subset['QNH'] <= qnhs[-1])]
-print(subset)
-print(subset.shape)
-subset = subset[subset['HORA'] == 11]
-print(subset)
-print(subset.shape)
-print('El promedio del QNH es: {:.2f}'.format(subset['QNH'].mean()))
-rows = subset.index.tolist()
-mega_lista = []
-for i in rows:
-	lista = []
-	for h in range(1, 13):
-		lista.append(data['QNH'][i+h])
-	mega_lista.append(lista)
-print('La longitud de la lista es {}'.format(len(mega_lista)))
-print('El promedio de la lista es: {:.2f}'.format(np.array(mega_lista).mean()))
-print(mega_lista)
-for i in range(len(mega_lista[0])):
-	suma = 0
-	for l in mega_lista:
-		suma += l[i]
-	print('El promedio de las posiciones {} en mega_lista es: {:.2f}'.format(i, suma / len(mega_lista)))
+def encuentra_posicion(archivo, patron):
+	for linea in archivo:
+		acierto = patron.search(linea)
+		if acierto:
+			linea = archivo.readline()
+			break
+			
+def remplaza_iguales(cadena):
+	for x in range(0,3):
+		cadena = cadena.replace("  "," ")
+		cadena = cadena.replace("==","=")
+		cadena = cadena.replace(" =","=")
+	return cadena
+			
+def acomoda_metares(archivo, linea, linea_ant):
+	try:
+		fecha = int(linea[0:11])
+		estado = 0        
+	except ValueError:
+		estado = 1
+	
+	igual = linea.count("=")
+	if estado == 0 and igual == 0:
+		linea_ant = linea[0:len(linea)-1]
+	elif estado == 1 and igual == 0:
+		linea_ant = linea_ant + linea[23:len(linea)-1]
+	elif estado == 0 and igual == 1:
+		linea = remplaza_iguales(linea)
+		linea = linea.replace("WS R", "WSR")
+		archivo.write(linea)
+#		print(linea)
+	elif estado == 1 and igual == 1:
+		linea_ant = linea_ant + linea[23:len(linea)-1]
+		linea_ant = remplaza_iguales(linea_ant)
+		linea_ant = linea_ant.replace("WS R", "WSR")
+		archivo.write(linea_ant + "\n")
+#		print(linea_ant)
+	return linea_ant
+
+hoy = datetime.today()
+anios = [x for x in range(2005, 2020)]
+meses = [y for y in range(1, 13)]
+for year in anios:
+	f3 = open('files/' + str(year) + '.txt', 'w')
+	for month in meses:
+		fecha = datetime(year, month, 1, 0, 0, 0)
+		anio = fecha.year
+		print(anio)
+		mes = convertir_a_string(fecha.month)
+		print(month)
+		dia_ini = convertir_a_string(fecha.day)
+		month_range = monthrange(fecha.year, fecha.month)
+		dia_fin = convertir_a_string(month_range[1])
+		print(dia_fin)
+
+		direccion = 'http://ogimet.com/display_metars2.php?lugar=MROC&tipo=SA&ord=DIR&nil=SI&fmt=txt&ano=%i&mes=%s&day=%s&hora=00&anof=%i&mesf=%s&dayf=%s&horaf=23&minf=59&enviar=Ver'
+		while True:
+			try:
+				url = direccion%(anio, mes, dia_ini, anio, mes, dia_fin)
+				r = urllib.request.urlopen(url)
+				texto = r.read().decode('iso-8859-1')
+				break
+			except:
+				sleep(30)
+				continue
+			
+
+		f1 = open('textow.txt', 'w')
+		f1.write(texto)
+		f1.close()
+
+		f2 = open('textow.txt', 'r')
+
+		formato = r'METAR/SPECI\sde\sMROC'
+		patron = re.compile(formato)
+		encuentra_posicion(f2, patron)
+		linea_ant = 'texto'
+		for linea in f2:
+			if linea[0] == '\n':
+				break
+			else:
+				linea_ant = acomoda_metares(f3, linea, linea_ant)
+		for x in range(300, 0, -1):
+			print(x)
+			sleep(1)
+		f2.close()
+	f3.close()
+
